246977564b1a1078c3c22517da5b71a274347732
Sandy_Arelis_Peña_10 <166466945+S2024E@users.noreply.github.com>
2024-04-22 11:43:22 -0500

    Update Ejercicio3.py

--- Practica_1/Ejercicio3.py
+++ Practica_1/Ejercicio3.py
@@ -1,7 +1,16 @@
 # Realice un programa que calcule la suma de los numeros hasta el valor ingresado
-a=1
-b=2
-c=3
-d=4
-e=5
-print(a+b+c+d+e)
+
+msg="""
+  1.Desea sumar los numeros
+  2.Desea restar los numeros
+"""
+print(msg)
+opcion=int(input("Ingresa una opcion"))
+a=int(input("Ingrese un numero"))
+
+if opcion==1:
+  print(a+b)
+elif opcion==2:
+  print(a-b)
+else:
+  print("Ingrese un valor valido")   
